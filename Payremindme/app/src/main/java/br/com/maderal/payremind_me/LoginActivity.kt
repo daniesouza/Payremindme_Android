043211e968c80e83819d70e3d846393862770434
Maderal15 <33066766+Maderal15@users.noreply.github.com>
2019-01-24 22:10:30 -0200

    comit

---
+++ Payremindme/app/src/main/java/br/com/maderal/payremind_me/LoginActivity.kt
@@ -0,0 +1,60 @@
+package br.com.maderal.payremind_me
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.app.LoaderManager.LoaderCallbacks
+import android.content.CursorLoader
+import android.content.Loader
+import android.database.Cursor
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.text.TextUtils
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+import java.util.ArrayList
+import android.Manifest.permission.READ_CONTACTS
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+
+    fun logar(view: View) {
+
+        val login = userName.text.toString()
+        val senha = password.text.toString()
+
+
+
+        if (login == "FIAP" && senha == "123") {
+
+
+        } else {
+
+            Toast.makeText(
+                this, "Usuário ou senha inválidos",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
