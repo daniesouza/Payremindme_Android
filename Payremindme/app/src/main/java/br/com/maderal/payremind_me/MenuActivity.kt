674991859463d2d94289c49e869ca7eb192c55fb
Maderal15 <33066766+Maderal15@users.noreply.github.com>
2019-01-26 21:44:55 -0200

    internacionalizacao

---
+++ Payremindme/app/src/main/java/br/com/maderal/payremind_me/MenuActivity.kt
@@ -0,0 +1,12 @@
+package br.com.maderal.payremind_me
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
